c032971c856bf5a6a6bee57505b22789d913b3e8
vincent vincent <vincent24alex3.com>
2023-09-22 13:57:26 +0700

    hampir

--- app/Http/Controllers/Admin/IzinController.php
+++ app/Http/Controllers/Admin/IzinController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\IzinRequest\IzinRequest;
+use App\Models\Congregation;
+use App\Models\CongregationAttendance;
+use App\Models\CongregationDiscipleshipDetail;
+use App\Models\DiscipleshipDetail;
 use App\Models\Izin;
 use Illuminate\Http\Request;
 
@@ -20,6 +24,14 @@ class IzinController extends Controller
 
     public function store(Request $request)
     {
+        $kegiatan = $request->kegiatan;
+        $nama = $request->nama;
+        $angkatan = $request->angkatan;
+        $month = $request->month == null ? date('m') : $request->month;
+        $year = $request->year == null ? date('Y') : $request->year;
+        $alasan = $request->alasan;
+        $tanggal = $request->tgl_kegiatan;
+
         $data = $request->validate([
             'nama' => ['required', 'string'],
             'angkatan' => ['required', 'string'],
@@ -30,11 +42,34 @@ class IzinController extends Controller
 
         $izin = Izin::create($data);
 
-        return redirect('thankyou');
+        if($kegiatan == 'Kebaktian'){   
+            $congregationId= Congregation::where(function ($x) use ($nama) {
+                $x->where('nama_lengkap', 'LIKE', '%' . $nama . '%')->get('congregation_id');
+            });
+    
+            $attendance[] = CongregationAttendance::whereCongregationId($congregationId)
+                ->whereYear('tanggal', $year)
+                ->whereMonth('tanggal', $month)
+                ->get();
+    
+            $congregationAttendance = CongregationAttendance::where('congregation_id', $congregationId)
+                                        ->where('tanggal', $tanggal)
+                                        ->first();
+    
+            if ($congregationAttendance != null) {
+                $congregationAttendance->update([
+                    'keterangan' => $alasan,
+                ]);
+            }
+        }
+        
+        return redirect('IzinKegiatan/thankyou');
     }
 
     public function thankyou()
     {
         return view('admin.auth.thankyou2');
     }
+
+    
 }
